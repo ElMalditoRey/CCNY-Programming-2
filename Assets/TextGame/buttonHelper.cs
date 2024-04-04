be35927c36d790d01afaaf795470904217b52cf4
MxGRRR <89796414+LeeMcGirr@users.noreply.github.com>
2024-04-04 16:48:16 -0400

    added singleton

--- Assets/TextGame/buttonHelper.cs
+++ Assets/TextGame/buttonHelper.cs
@@ -5,10 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class buttonHelper : MonoBehaviour
 {
+    public textGameManager myMgr;
+
+    public void Start()
+    {
+        myMgr = FindObjectOfType<textGameManager>();
+    }
     //little script to change to a new scene
     public void GoToScene(string sceneName)
     {
         Debug.Log("Pressed the button!");
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void AddToInventory(string item)
+    {
+        myMgr.myInventory.Add(item);
+    }
+
+    public void EnableObject(GameObject target)
+    {
+        target.SetActive(true);
+    }
+
+    public void SetName(string name)
+    {
+        myMgr.name = name;
     }
 }
--- Assets/TextGame/textGameManager.cs
+++ Assets/TextGame/textGameManager.cs
@@ -7,6 +7,39 @@ using UnityEngine.SceneManagement;
 public class textGameManager : MonoBehaviour
 {
 
+    //-------------------------------------------------- code for instancing a singleton and loading before the scene -----------//
+
+    private static textGameManager _MgrInstance; //declaring that an instance of this script exists
+
+    public static textGameManager myInstance { //the constructor function, this described a way to get or spawn the game manager whenever it is called
+
+        get //this get function will run once a textGameManager instance is declared in any of our code
+        {
+
+            if(_MgrInstance == null) //if there is no actual instance in scene yet (private mgr == null), then spawn a gameobject
+            {
+                GameObject myGO = new GameObject("GameManager");
+                myGO.AddComponent<textGameManager>();
+                DontDestroyOnLoad(myGO);
+            }
+            return _MgrInstance; //return a valid game manager
+        }   
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)] //the following method runs before the scene loads
+    public static void InitializeManager()
+    {
+        textGameManager newMgr = textGameManager.myInstance;
+    }
+
+    // ----------------------------------------- end -----------------------------//
+
+
+
+
+
+    public List<string> myInventory;
+
     public TMP_InputField myInput;
     public string playerName;
 
